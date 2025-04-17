42484914ed0c8ed10d21ead80909e9e267921d34
Alviam564 <alvarezwilliam543@gmail.com>
2025-04-16 23:01:45 -0400

    Progress

---
+++ Pokemon-TCG/pages/set10.js
@@ -0,0 +1,1 @@
+import queryBuilder from './queryBuilder.js';
---
+++ Pokemon-TCG/src/configure.js
@@ -0,0 +1,7 @@
+const configuration = {
+    API_KEY: "168a95bd-b753-4362-81c4-c8483249906d",
+    BASE_URL: "https://api.pokemontcg.io/v2"
+};
+
+
+export default configuration
