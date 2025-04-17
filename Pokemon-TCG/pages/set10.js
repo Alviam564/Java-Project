88f28975d0e82442dea54039c65f2e62a69533c3
Alviam564 <alvarezwilliam543@gmail.com>
2025-04-16 23:47:01 -0400

    progress

--- Pokemon-TCG/pages/set10.js
+++ Pokemon-TCG/pages/set10.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 import queryBuilder from './queryBuilder.js';
-=======
-import queryBuilder from './queryBuilder.js';
->>>>>>> 42484914ed0c8ed10d21ead80909e9e267921d34
--- Pokemon-TCG/src/configure.js
+++ Pokemon-TCG/src/configure.js
@@ -1,9 +1,5 @@
 const configuration = {
-<<<<<<< HEAD
     API_KEY: "",
-=======
-    API_KEY: "168a95bd-b753-4362-81c4-c8483249906d",
->>>>>>> 42484914ed0c8ed10d21ead80909e9e267921d34
     BASE_URL: "https://api.pokemontcg.io/v2"
 };
 
