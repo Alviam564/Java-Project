637f0f844b15284bf1b71348c79228f266af7a62
Alviam564 <alvarezwilliam543@gmail.com>
2025-04-25 11:37:13 -0400

    update cause why not

--- Pokemon-TCG/pages/set10.js
+++ Pokemon-TCG/pages/set10.js
@@ -1,36 +1,44 @@
 fetch('../src/data/unbroken-bonds.json')
-  .then(response => response.json())
-  .then(data => {
-    displayCards(data);
-  })
-  .catch(error => console.error("Failed to load JSON:", error));
+.then(response => response.json())
+.then(data => {
+  displayCards(data);
+})
+.catch(error => console.error("Failed to load JSON:", error));
 
-  function displayCards(cards) {
-    const container = document.getElementById('card-container');
-    container.innerHTML = '';
+function openMenu() {
+  document.body.classList += " menu--open"
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu--open')
+}
+
+function displayCards(cards) {
+  const container = document.getElementById('card-container');
+  container.innerHTML = '';
+  
+  cards.forEach(card => {
+    const cardDiv = document.createElement('div');
+    cardDiv.classList.add('card-UB');
     
-    cards.forEach(card => {
-      const cardDiv = document.createElement('div');
-      cardDiv.classList.add('card-UB');
+    cardDiv.innerHTML = `
+      <h3>${card.name}</h3>
+      <img src="${card.images.small}" alt="${card.name}">
+      <p><strong>Type:</strong> ${card.types}</p>
+      <p><strong>Rarity:</strong> ${card.rarity}</p>
+      <p><strong>Set:</strong> ${card.set.name}</p>
+      <p><strong>Low Price:</strong> ${card.prices?.[0]?.normal?.low || "N/A"}</p>
+      <p><strong>High Price:</strong> ${card.prices?.[0]?.normal?.high || "N/A"}</p>
+      <p><strong>Market Price:</strong> ${card.prices?.[0]?.normal?.market || "N/A"}</p>
+  
+      <p><strong>Low Price:</strong> ${card.prices?.[1]?.reverseHolofoil?.low || "N/A"}</p>
+      <p><strong>High Price:</strong> ${card.prices?.[1]?.reverseHolofoil?.high || "N/A"}</p>
+      <p><strong>Market Price:</strong> ${card.prices?.[1]?.reverseHolofoil?.market || "N/A"}</p>
       
-      cardDiv.innerHTML = `
-        <h3>${card.name}</h3>
-        <img src="${card.images.large}" alt="${card.name}">
-        <p><strong>Type:</strong> ${card.types}</p>
-        <p><strong>Rarity:</strong> ${card.rarity}</p>
-        <p><strong>Set:</strong> ${card.set.name}</p>
-        <p><strong>Low Price:</strong> ${card.prices?.[0]?.normal?.low || "N/A"}</p>
-        <p><strong>High Price:</strong> ${card.prices?.[0]?.normal?.high || "N/A"}</p>
-        <p><strong>Market Price:</strong> ${card.prices?.[0]?.normal?.market || "N/A"}</p>
-    
-        <p><strong>Low Price:</strong> ${card.prices?.[1]?.reverseHolofoil?.low || "N/A"}</p>
-        <p><strong>High Price:</strong> ${card.prices?.[1]?.reverseHolofoil?.high || "N/A"}</p>
-        <p><strong>Market Price:</strong> ${card.prices?.[1]?.reverseHolofoil?.market || "N/A"}</p>
-        
-        <p><strong>Low Price:</strong> ${card.prices?.[0]?.holofoil?.low || "N/A"}</p>
-        <p><strong>High Price:</strong> ${card.prices?.[0]?.holofoil?.high || "N/A"}</p>
-        <p><strong>Market Price:</strong> ${card.prices?.[0]?.holofoil?.market || "N/A"}</p>`;
-    
-      container.appendChild(cardDiv);
-      });
-    }
+      <p><strong>Low Price:</strong> ${card.prices?.[0]?.holofoil?.low || "N/A"}</p>
+      <p><strong>High Price:</strong> ${card.prices?.[0]?.holofoil?.high || "N/A"}</p>
+      <p><strong>Market Price:</strong> ${card.prices?.[0]?.holofoil?.market || "N/A"}</p>`;
+  
+    container.appendChild(cardDiv);
+  });
+}
--- Pokemon-TCG/pages/set9.js
+++ Pokemon-TCG/pages/set9.js
@@ -1,37 +1,93 @@
+let cardsData = []
+
 fetch('../src/data/team-up.json')
-.then(response => response.json())
-.then(data => {
-  displayCards(data);
-})
-.catch(error => console.error("Failed to load JSON:", error)
-);
+  .then(response => response.json())
+  .then(data => {
+    displayCards(data);
+    cardsData = data;
+    createBackgroundCards()
+  }
+)
+.catch(error => console.error("Failed to load JSON:", error));
+
+function openMenu() {
+  document.body.classList += " menu--open"
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu--open')
+}
+
+function createBackgroundCards() {
+  const container = document.querySelector(".background-cards");
+
+  for (let i = 0; i < 7; i++) {
+    const card = document.createElement("div")
+    card.classList.add("card")
+
+    const img = document.createElement("img")
+    img.src = getRandomCardImage()
+
+    card.appendChild(img)
+    container.appendChild(card)
+
+    setInterval(() => {
+      img.src = getRandomCardImage()
+    }, 6000);
+  }
+}
+
+function getRandomCardImage() {
+  if(!Array.isArray(cardsData) || cardsData.length === 0) {
+    return "../src/assets/Pokémon_Trading_Card_Game_logo.svg.png"
+  }
+  const randomIndex = Math.floor(Math.random() * cardsData.length)
+  return cardsData[randomIndex].images.large;
+}
+
+function getFormattedPrices(pricesArr) {
+  const priceTypes = ["normal", "reverseHolofoil", "holofoil"]
+  const labels = {
+    normal : "Normal",
+    reverseHolofoil: "Reverse Holofoil",
+    holofoil: "Holofoil"
+  };
+
+  let cardtypes = ""
+
+  for (const priceGroup of pricesArr) {
+    for (const type of priceTypes) {
+      if (priceGroup[type]) {
+        const low = priceGroup[type].low ?? "N/A";
+        const high = priceGroup[type].high ?? "N/A";
+        cardtypes += `
+        <p><strong>${labels[type]} Low:</strong> ${low}</p>
+        <p><strong>${labels[type]} High:</strong> ${high}</p>
+        `;
+      }
+    }
+  }
+
+  return cardtypes
+}
 
 function displayCards(cards) {
-const container = document.getElementById('card-container');
-container.innerHTML = '';
-
-cards.forEach(card => {
-  const cardDiv = document.createElement('div');
-  cardDiv.classList.add('card-TU');
+  const container = document.getElementById('card-container');
+  container.innerHTML = '';
   
-  cardDiv.innerHTML = `
-    <h3>${card.name}</h3>
-    <img src="${card.images.large}" alt="${card.name}">
-    <p><strong>Type:</strong> ${card.types}</p>
-    <p><strong>Rarity:</strong> ${card.rarity}</p>
-    <p><strong>Set:</strong> ${card.set.name}</p>
-    <p><strong>Low Price:</strong> ${card.prices?.[0]?.normal?.low || "N/A"}</p>
-    <p><strong>High Price:</strong> ${card.prices?.[0]?.normal?.high || "N/A"}</p>
-    <p><strong>Market Price:</strong> ${card.prices?.[0]?.normal?.market || "N/A"}</p>
-
-    <p><strong>Low Price:</strong> ${card.prices?.[1]?.reverseHolofoil?.low || "N/A"}</p>
-    <p><strong>High Price:</strong> ${card.prices?.[1]?.reverseHolofoil?.high || "N/A"}</p>
-    <p><strong>Market Price:</strong> ${card.prices?.[1]?.reverseHolofoil?.market || "N/A"}</p>
+  cards.forEach(card => {
+    const cardDiv = document.createElement('div');
+    cardDiv.classList.add('card-TU');
     
-    <p><strong>Low Price:</strong> ${card.prices?.[0]?.holofoil?.low || "N/A"}</p>
-    <p><strong>High Price:</strong> ${card.prices?.[0]?.holofoil?.high || "N/A"}</p>
-    <p><strong>Market Price:</strong> ${card.prices?.[0]?.holofoil?.market || "N/A"}</p>`;
-
-  container.appendChild(cardDiv);
-  });
+    cardDiv.innerHTML = `
+    <h3>${card.name}</h3>
+    <img src="${card.images.small}" alt="${card.name}">
+      <p><strong>Type:</strong> ${card.types}</p>
+      <p><strong>Rarity:</strong> ${card.rarity}</p>
+      <p><strong>Set:</strong> ${card.set.name}</p>
+      ${getFormattedPrices(card.prices)}
+    `;
+    container.appendChild(cardDiv);
+    }
+  );
 }
